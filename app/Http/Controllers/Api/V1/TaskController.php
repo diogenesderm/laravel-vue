50878e30c81a9e20056b2eac0e5d49dcc0525466
Diógenes Estevão <diogenestotal@gmail.com>
2024-03-11 14:15:57 -0300

    Crud tasks e Login por api

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return TaskResource::collection(Task::all());;
+    }
+
+    public function show(Task $task)
+    {
+        return TaskResource::make($task);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+        return TaskResource::make($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return TaskResource::make($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\StoreTaskRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends StoreTaskRequest
+{
+   
+}
